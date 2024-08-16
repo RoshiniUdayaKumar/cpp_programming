42d4d3004de4e2bd9d5354207a8d04b8cc92c475
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2024-08-16 14:23:06 -0500

    Add files via upload

---
+++ Numbers/Ex4.cpp
@@ -0,0 +1,12 @@
+//C++ Program to Display Characters from A to Z Using Loop.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char character;
+    for (character='A'; character <='Z'; character ++)
+    {
+        cout<<character<<endl;
+    }
+    return 0;
+}
